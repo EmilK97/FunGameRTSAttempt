9fc8020ed7ae9cdf289f5c81d80c0ef30ab79145
emilk@DAMBO <emil.klonowski@s3connectedhealth.com>
2024-08-12 15:56:28 +0200

    Added garrison to cities

--- Entities/City/city.py
+++ Entities/City/city.py
@@ -3,6 +3,8 @@ from random import randint
 
 import pygame
 from Entities.Map.tile import Tile
+from Entities.Unit.squad import Garrison
+from Entities.Unit.trooper import Trooper
 from Enums.factions import Factions
 from Enums.races import Races
 from settings import CITY_ICON
@@ -17,6 +19,7 @@ class City(pygame.sprite.Sprite):
     id: int
     tile_location: Tile
     image_path: str
+    _garrison: Garrison
 
     def __init__(
         self,
@@ -37,9 +40,11 @@ class City(pygame.sprite.Sprite):
         self._current_hp = self._max_hp
         self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect()
+        # Initialize empty Garrison
+        self._garrison = Garrison(starting_tile=self.tile_location)
 
     def __str__(self):
-        return f"City {self.name}, id: {self.id}, HP: {self.current_hp}/{self._max_hp}"
+        return f"City {self.name}, id: {self.id}, HP: {self.current_hp}/{self._max_hp}\n{str(self._garrison)}"
 
     @property
     def current_hp(self) -> int:
@@ -55,7 +60,31 @@ class City(pygame.sprite.Sprite):
         logging.info(f"City {str(self)} was destroyed!!!\n")
         del self
 
+    @property
+    def garrison(self) -> Garrison:
+        return self._garrison
+
+    @property
+    def is_garrison_populated(self):
+        return bool(len(self._garrison))
+
+    def add_troopers_to_garrison(self, *troopers: Trooper):
+        self._garrison.extend(*troopers)
+
+    def remove_troopers_from_garrison(self, *troopers: Trooper):
+        for trooper in troopers:
+            self._garrison.remove(trooper)
+
 
 class CapitalCity(City):
+    def __init__(
+        self, *groups, race: Races, faction: Factions, name: str, tile_location: Tile
+    ):
+        super().__init__(
+            *groups, race=race, faction=faction, name=name, tile_location=tile_location
+        )
+        self._max_hp = 30
+        self._current_hp = self._max_hp
+
     def __str__(self):
         return f"Capital {super().__str__()}"
--- Entities/Unit/squad.py
+++ Entities/Unit/squad.py
@@ -2,11 +2,14 @@ import logging
 
 import pygame
 from Entities.Map.tile import Tile
+from Entities.Unit.trooper import Trooper
 from Enums.exceptions import SquadSizeLimitReach
 from settings import SQUAD_ICON
 
 
 class Squad(list, pygame.sprite.Sprite):
+    """Can be stationed only on map as  a separate unit."""
+
     SIZE_LIMIT = 8
 
     def __init__(self, *args, starting_tile: Tile, image_path: str = SQUAD_ICON):
@@ -28,11 +31,21 @@ class Squad(list, pygame.sprite.Sprite):
     def __str__(self):
         return f"Location: {self._tile_location}, units: {[str(_) for _ in self]}"
 
-    def append(self, __object):
+    def append(self, __object: Trooper):
         if len(self) >= self.SIZE_LIMIT:
             raise SquadSizeLimitReach
         super().append(__object)
 
+    def extend(self, *troopers: Trooper):
+        if len(self) + len(troopers) >= self.SIZE_LIMIT:
+            raise SquadSizeLimitReach
+        super().extend(troopers)
 
-class Garnison(Squad):
+
+class Garrison(Squad):
+    """Can be stationed only in City."""
+
     SIZE_LIMIT = 16
+
+    def __str__(self):
+        return f"Garrison: {[str(_) for _ in self]}"
